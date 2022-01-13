4fc8738c04301056a09f82a8d93070b3cf476a95
al2698 <mr20166@gmail.com>
2022-01-14 06:52:37 +0800

    0652

--- app1.js
+++ app1.js
@@ -47,5 +47,5 @@ async function create(ctx) {
   }
 }
 
-console.log('Server run at http://127.0.0.1:8000')
-await app.listen({ port: 8000 });
+console.log('Server run at http://127.0.0.1:8032')
+await app.listen({ port: 8032});
